953e1e0c791c7710e1ca3de3ac81d9fb1bc69ea3
tudan110 <wtudan@126.com>
2019-11-23 20:44:08 +0800

    init

---
+++ src/main/java/indi/tudan/spider/LearnSpiderApplication.java
@@ -0,0 +1,13 @@
+package indi.tudan.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indi/tudan/spider/LearnSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.tudan.spider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
